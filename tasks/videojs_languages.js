9abe82f457b3ec5b83ef292d1908af77ce31ad56
Tom Johnson <seniorflexdeveloper@gmail.com>
2014-08-26 13:07:38 -0700

    intial commit

---
+++ tasks/videojs_languages.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('videojs_languages', 'A Grunt plugin for compiling VideoJS language assets.', function() {
+
+    this.files.forEach(function(f) {
+      var languageName = path.basename(f.src, '.json');
+      var languageData = grunt.file.readJSON(f.src);
+      var jsTemplate = 'videojs.addLanguage("' + languageName + '",' + JSON.stringify(languageData) + ');';
+      var jsFilePath = path.join(f.dest, languageName + '.js');
+      grunt.file.write(jsFilePath, jsTemplate);
+      grunt.log.writeln('- [' + languageName +'] Language Built. File "' + jsFilePath + '" created.');
+    });
+  });
+};
