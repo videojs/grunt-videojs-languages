99c8650779cf8462e07f9ec9023bfda932f60204
Tom Johnson <seniorflexdeveloper@gmail.com>
2014-08-27 16:12:54 -0700

    refactor code

--- tasks/videojs_languages.js
+++ tasks/videojs_languages.js
@@ -5,28 +5,31 @@ var util = require('util');
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('vjslanguages', 'A Grunt plugin for compiling VideoJS language assets.', function() {
+
+    var createLanguageFile = function(languageName, languageData, jsFilePath) {
+      var jsTemplate = 'videojs.addLanguage("' + languageName + '",' + JSON.stringify(languageData,null,' ') + ');';
+
+      grunt.file.write(jsFilePath, jsTemplate);
+      grunt.log.writeln('- [' + languageName +'] Language Built. File "' + jsFilePath + '" created.');
+    };
+
     this.files.forEach(function(f) {
-      var languageName, languageData, jsTemplate, jsFilePath;
+      var languageName, languageData, jsFilePath;
       // Multiple Files Case
       if(util.isArray(f.src)){
         for(var i =0; i < f.src.length; i++) {
-          console.log('-', f.src[i], f.dest);
           languageName = path.basename(f.src[i], '.json');
           languageData = grunt.file.readJSON(f.src[i]);
-          jsTemplate = 'videojs.addLanguage("' + languageName + '",' + JSON.stringify(languageData,null,' ') + ');';
           jsFilePath = path.join(f.dest, languageName + '.js');
-          grunt.file.write(jsFilePath, jsTemplate);
-          grunt.log.writeln('- [' + languageName +'] Language Built. File "' + jsFilePath + '" created.');
+          createLanguageFile(languageName, languageData, jsFilePath);
         }
       }
       // Singular File Case
       else {
         languageName = path.basename(f.src, '.json');
         languageData = grunt.file.readJSON(f.src);
-        jsTemplate = 'videojs.addLanguage("' + languageName + '",' + JSON.stringify(languageData,null,' ') + ');';
         jsFilePath = path.join(f.dest, languageName + '.js');
-        grunt.file.write(jsFilePath, jsTemplate);
-        grunt.log.writeln('- [' + languageName +'] Language Built. File "' + jsFilePath + '" created.');
+        createLanguageFile(languageName, languageData, jsFilePath);
       }
     });
   });
